de8d4f761e536efe6d30b54fbf81f149cd01eec4
盛凯 <80961244@yonghui.cn>
2020-01-20 11:22:50 +0800

    init

---
+++ src/main/java/com/shengkai/learnjenkins/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.shengkai.learnjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shengkai/learnjenkins/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shengkai.learnjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
